ada190505602b0ee32d5c4062b2d34ceec4e07a2
jaumep1 <76637874+jaumep1@users.noreply.github.com>
2021-07-12 10:11:38 -0700

    Connected to parse and added model classes

---
+++ app/src/main/java/com/example/fbu_final_project/ParseApplication.java
@@ -0,0 +1,28 @@
+package com.example.fbu_final_project;
+
+import android.app.Application;
+
+import com.example.fbu_final_project.models.Event;
+import com.example.fbu_final_project.models.User;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Register parse models
+        ParseObject.registerSubclass(User.class);
+        ParseObject.registerSubclass(Event.class);
+
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+    }
+}
---
+++ app/src/main/java/com/example/fbu_final_project/models/Event.java
@@ -0,0 +1,9 @@
+package com.example.fbu_final_project.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("Event")
+public class Event extends ParseObject {
+
+}
