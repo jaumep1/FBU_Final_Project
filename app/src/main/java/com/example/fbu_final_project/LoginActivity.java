47930bdc85a030a0db0fc13cb60bac1fb759cac2
jaumep1 <76637874+jaumep1@users.noreply.github.com>
2021-07-14 09:33:53 -0700

    Completed login activity

--- app/src/main/java/com/example/fbu_final_project/LoginActivity.java
+++ app/src/main/java/com/example/fbu_final_project/LoginActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+
+    EditText etUsername;
+    EditText etPassword;
     Button btnLogin;
 
     @Override
@@ -17,13 +28,41 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnLogin = findViewById(R.id.btnLogin);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(i);
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
             }
         });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user: " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue on login", e);
+                    Toast.makeText(LoginActivity.this, "Invalid Login!", Toast.LENGTH_SHORT).show();
+
+                    return;
+                }
+
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
