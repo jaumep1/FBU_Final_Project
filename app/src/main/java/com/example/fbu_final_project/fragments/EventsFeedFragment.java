c37b3282e39bd7ac9472564a62078c3186a61a3f
jaumep1 <76637874+jaumep1@users.noreply.github.com>
2021-07-16 14:56:47 -0700

    Implemented searching by query

--- app/src/main/java/com/example/fbu_final_project/fragments/EventsFeedFragment.java
+++ app/src/main/java/com/example/fbu_final_project/fragments/EventsFeedFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,9 +13,13 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fbu_final_project.R;
 import com.example.fbu_final_project.adapters.EventsFeedAdapter;
 import com.example.fbu_final_project.databinding.FragmentEventsFeedBinding;
 import com.example.fbu_final_project.models.Event;
@@ -43,7 +49,6 @@ public class EventsFeedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         binding = FragmentEventsFeedBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -51,6 +56,8 @@ public class EventsFeedFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        setHasOptionsMenu(true);
 
         LinearLayoutManager manager = new LinearLayoutManager(getContext());
 
@@ -84,6 +91,78 @@ public class EventsFeedFragment extends Fragment {
                 android.R.color.holo_red_light);
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        MenuItem searchItem = menu.findItem(R.id.miSearch);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                filterEvents(query);
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                queryPosts();
+                return true;
+            }
+        });
+    }
+
+    private void filterEvents(String search) {
+        ParseQuery<Event> queryName = ParseQuery.getQuery(Event.class);
+        ParseQuery<Event> queryDescription = ParseQuery.getQuery(Event.class);
+        ParseQuery<Event> queryAuthor = ParseQuery.getQuery(Event.class);
+
+        queryName.whereContains(Event.KEY_EVENT_NAME, search);
+        queryDescription.whereContains(Event.KEY_EVENT_DESCRIPTION, search);
+        queryAuthor.whereContains(Event.KEY_AUTHOR, search);
+
+        List<ParseQuery<Event>> queries = new ArrayList<>();
+        queries.add(queryName);
+        queries.add(queryDescription);
+        queries.add(queryAuthor);
+
+
+        ParseQuery<Event> mainQuery = ParseQuery.or(queries);
+        mainQuery.include(Event.KEY_EVENT_NAME);
+        mainQuery.include(Event.KEY_EVENT_DESCRIPTION);
+        mainQuery.include(Event.KEY_AUTHOR);
+        mainQuery.include(Event.KEY_START_TIME);
+        mainQuery.include(Event.KEY_END_TIME);
+
+        mainQuery.setLimit(20);
+        mainQuery.addDescendingOrder("createdAt");
+        mainQuery.findInBackground(new FindCallback<Event>() {
+            @Override
+            public void done(List<Event> feed, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                events.clear();
+                events.addAll(feed);
+                adapter.notifyDataSetChanged();
+                Log.i("waka", events.toString());
+            }
+        });
+    }
+
     protected void queryPosts(){
         ParseQuery<Event> query = ParseQuery.getQuery(Event.class);
 
@@ -102,6 +181,7 @@ public class EventsFeedFragment extends Fragment {
                     Log.e(TAG, "Issue with getting posts", e);
                     return;
                 }
+                events.clear();
                 events.addAll(feed);
                 adapter.notifyDataSetChanged();
             }
