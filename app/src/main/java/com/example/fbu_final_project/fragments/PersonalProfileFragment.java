63548a178ae86644e4ebbd8b2f6051c436a713c0
jaumep1 <76637874+jaumep1@users.noreply.github.com>
2021-08-02 10:36:19 -0700

    Implemented toggle between attendee/creator profile views

--- app/src/main/java/com/example/fbu_final_project/fragments/PersonalProfileFragment.java
+++ app/src/main/java/com/example/fbu_final_project/fragments/PersonalProfileFragment.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -113,6 +114,18 @@ public class PersonalProfileFragment extends Fragment {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        binding.switchProfileType.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isAttendee = !isAttendee;
+                setUser();
+                try {
+                    loadEventsFromCacheData(MainActivity.getEvents());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
@@ -175,7 +188,11 @@ public class PersonalProfileFragment extends Fragment {
             }
         });
 
-        binding.tvHeader.setText("Events You Created:");
+        if (isAttendee) {
+            binding.tvHeader.setText("Events You Are Going To:");
+        } else {
+            binding.tvHeader.setText("Events You Created:");
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -288,6 +305,7 @@ public class PersonalProfileFragment extends Fragment {
 
                 events.add(newEvent);
                 activeEvents.add(newEvent);
+                adapter.notifyDataSetChanged();
             }
         }
     }
